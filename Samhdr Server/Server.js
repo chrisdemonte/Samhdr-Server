a29e290b92a2eb8ce1e243361e3551d3367f0708
chrisdemonte <46972602+chrisdemonte@users.noreply.github.com>
2023-01-13 15:44:30 -0500

    Accept and Deny Matches
    Server side accepting and denying matches works as planned
    Client side has bugs
    Added Comments to server code

--- Samhdr Server/Server.js
+++ Samhdr Server/Server.js
@@ -1,92 +1,108 @@
-const http = require('http')
+//const http = require('http')
 const port = 22222
 const io = require('socket.io')(port, {cors : {origin : "*"}})
-const url = require('url')
-//const room = require("./Room.js")
-//import Player from "Player.js"
+//const url = require('url')
 
+/**
+ * An array of player data for the sockets connect to the server
+ * Struct { socketId : string, playerName : string, state: int}
+ * state : {0 = not ready, 1 = ready, 2 = in a match}
+ */
 var players = []
 
-function prepareForMatch(id, name){
+//Updates the player information for a given socket id
+function updatePlayer(id, name, state){
     for (let i = 0; i < players.length; i++){
         if (players[i].id == id){
             players[i].name = name
-            players[i].ready = true
+            players[i].state = state
         }
     }
 }
 
+/**
+ * On Connection:
+ * - The server creates a spot for the client on the player list
+ * - Server side listeners are added to the connected socket for all socket events
+ */
 io.on("connection", socket =>{
     //console.log(socket.id)
-    players.push({"id" : socket.id, "name":"", "ready": false})
+    //adding the socket to the socket list, but setting it in a "not ready" state
+    players.push({"id" : socket.id, "name":"", "state": 0})
+
+    // On Message: test event that console.logs a message from the socket
     socket.on("message", message => {
-        console.log(message)
-        
+        console.log(message)    
     })
     
+    /**
+     * On Join Room:
+     * - Client-side the player has picked a name and set their card selection
+     * - Server updates the player's state in the player list to a "ready" state
+     * and sends an updated player list to all sockets connected to the server 
+     */ 
     socket.on("join-room", name =>{
-
-        prepareForMatch(socket.id, name)
         console.log(players)
-       // io.to(socket).emit("room-data", players)
+        updatePlayer(socket.id, name, 1)
         io.emit("room-data", players)
     })
+
+    /**
+     * On Send-Challenge:
+     * - Client-side a player has selected another player to challenge
+     * - Recieve the name of the challenger and the socket ID of the player
+     * being challenged
+     * - Emit a "recieve-challenge" message to the player being challenged
+     */
     socket.on("send-challenge", (challengerName, targetId) =>{
-        //console.log(data)
-        //console.log("incoming challenge for " + data.id + " from " + data.name)
         io.in(targetId).emit("recieve-challenge", challengerName, socket.id)
     })
+
+    /**
+     * On Accept-Challenge:
+     * -Event takes in the name of the challenger and the name and id of the
+     * player being challenged
+     * -Client-side the player who was challenged accepted the challenge
+     * -Generate a random number between 1 and 0 to select who moves first during a game
+     * -Send a challenge-accepted message to both players
+     * -Update the player list and send a message to all sockets
+     */
+    socket.on("accept-challenge", (challengeeName, challengerName, challengerID) =>{
+
+        //generate a random number (0 - 1): 
+        // 0 challengee moves second, 1 challengee moves first
+        let challengeeMovesFirst = Math.floor(Math.random * 2)
+        //set challengerMoves first to the opposite value of challengeeMovesFirst
+        let challengerMovesFirst = (challengeeMovesFirst + 1) % 2
+
+        //send the challenge accepted message to the challenger
+        io.in(challengerID).emit("challenge-accepted", challengeeName, socket.id, challengerMovesFirst)
+        //send the challenge accepted message to the person who was challenged
+        io.in(socket.id).emit("challenge-accepted", challengerName, challengerID, challengeeMovesFirst)
+
+        //set the player data to reflect that they are in a match
+        updatePlayer(socket.id, challengeeName, 2)
+        updatePlayer(challengerID, challengerName, 2)
+
+        //update all clients' room data
+        io.emit("room-data", players)
+    })
+
+    /**
+     * On deny-challenge:
+     * - CLient-side the player being challenged denied the challenge request
+     * - Send a message to the challenger that their request was denied
+     * - Send a message to the denier to update their room
+     */
+    socket.on("deny-challenge", (denyerName, challengerName, challengerId) =>{
+
+        //sending message to the denied challenger
+        io.in(challengerId).emit("challenge-denied", denyerName, socket.id)
+        //sending message to the player who denied the challenge
+        io.in(socket.id).emit("remove-challenger")
+    })
+
     //io.to(socket.id).emit("success")
     
 })
 
-
-
-/*
-var rooms = []
-
-function addRoom(player, pw){
-    let place = rooms.length
-    rooms.push({index: place, player1 : player, player2 : null, password : pw})
-    return rooms[place]
-}*/
-/*
-const server = http.createServer(function(req, res){
-    //console.log(req.url)
-    request = url.parse(req.url, true)
-    pathname = request.pathname
-    query = request.query
-    //console.log(pathname)
-    //console.log(query.name)
-
-    if(pathname == "/connect"){
-        console.log("establishing connection")
-        res.writeHead(200, {'Conetent-Type': 'application/json', 'Access-Control-Allow-Origin' : '*'})
-       // res.write("{message: \"Connected to server successfully.\"} ")
-       // res.end()
-        res.end("{\"message\": \"Connected to server successfully.\" }")
-    }
-    else if(pathname == "/join"){
-        let room = addRoom(query.name, query.password)
-        res.writeHead(200, {'Conetent-Type': 'text/plain'})
-        res.write("You have joined room " + (room.index + 1))
-        res.write("\n")
-        res.write(JSON.stringify(room))
-        res.end()
-        console.log(rooms)
-    }
-    else {
-        res.writeHead(404, {'Conetent-Type': 'text/plain'})
-        res.write("The resource you are looking for does not exist")
-        res.end() 
-    }
-})
-
-server.listen(port, function(error){
-    if (error){
-        console.log("Server error:", error)
-    }
-    else {
-        console.log("Server is listening on port " + port)
-    }
-})*/
